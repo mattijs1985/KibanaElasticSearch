ff5a3ee1a62d01b98b35b0d0f5734fa1b43c880a
Mattijs - ME.COM <mattijsvantorre@me.com>
2022-08-08 16:22:57 +0200

    Create new project

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Elastic.Kibana.Serilog.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
